cbeac7f8199601fae4c03dd2a366d90b0fe09f3b
adrianajani <168966483+adrianajani@users.noreply.github.com>
2024-05-11 22:11:23 +1000

    add code to JaveScript file

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,42 @@
-//const topics = "HTML, CSS, Git, JavaScript";
+const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 //console.log(topics);
 
-//const topic = "HTML";
-//
-//if (topic === 'HTML') {
-//  console.log("Let's study HTML!");
-//} else if (topic === 'CSS') {
-//  console.log("Let's study CSS!");
-//} else if (topic === 'Git') {
-//  console.log("Let's study Git!");
-//} else if (topic === 'JavaScript') {
-//  console.log("Let's study JavaScript!");
-//} else {
-//  console.log('Please try again!');
-//}
+function listTopics() {
+    for (let x = 0; x < topics.length; x++) {
+      console.log(topics[x]);
+    }
+  }
+
+
+  function selectTopic() {
+    if (topics === 'HTML') {
+      console.log("Let's study HTML!");
+    } else if (topics === 'CSS') {
+      console.log("Let's study CSS!");
+    } else if (topics === 'Git') {
+      console.log("Let's study Git!");
+    } else if (topics === 'JavaScript') {
+      console.log("Let's study JavaScript!");
+    } else {
+      console.log('Please try again!');
+    }
+  }
+
+  console.log('Here are the topics we learned through Prework:');
+  listTopics();
+  console.log('Which topic should we study first?');
+  selectTopic();
+
 
 //const shapes = ["triangle", "square", "pentagon", "circle"];
 //for(let x = 0; x < shapes.length; x++) {
 // console.log(shapes[x]);
 //}
 
+/*
 const topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
 for(let x = 0; x < topics.length; x++) {
     console.log(topics[x]);
 }
+*/
 
